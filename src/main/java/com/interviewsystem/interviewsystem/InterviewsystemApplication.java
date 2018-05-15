42ca80936bd2374b28fc48e66a0f15591a1e678f
poonam <poonam_chate@rediffmail.com>
2018-05-15 11:08:38 +0530

    Code base creation.

---
+++ src/main/java/com/interviewsystem/interviewsystem/InterviewsystemApplication.java
@@ -0,0 +1,12 @@
+package com.interviewsystem.interviewsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewsystemApplication.class, args);
+	}
+}
